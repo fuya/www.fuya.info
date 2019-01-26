afe9e057255635ddae683cb34573d9a38fa00dbe
Fuya <dev@fuya.info>
2019-01-26 13:33:55 +0900

    contentful sample page

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,28 @@
 <template>
   <section class="container">
-    <span v-html="r" />
+    {{ entry.fields.title }}
   </section>
 </template>
 
 <script>
-import Prismic from 'prismic-javascript'
-import PrismicDOM from 'prismic-dom'
-
-const endpoint = 'https://fuya.prismic.io/api/v2'
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
 
 export default {
   components: {},
-  computed: {
-    r() {
-      return this.res && PrismicDOM.RichText.asHtml(this.res.data.title)
+  head() {
+    return {
+      title: this.entry,
+      meta: [
+        { hid: 'description', name: 'description', content: this.entry },
+        { hid: 'og:image', name: 'og:image', content: this.entry }
+      ]
     }
   },
-  asyncData({ params }) {
-    return Prismic.getApi(endpoint, {})
-      .then(api => api.getSingle('top_page'))
-      .then(response => ({
-        res: response
-      }))
+  asyncData() {
+    return client.getEntry('1Fgho4wnhcjP4XW7nDHjU5').then(entry => ({
+      entry: entry
+    }))
   }
 }
 </script>
---
+++ plugins/contentful.js
@@ -0,0 +1,12 @@
+const contentful = require('contentful')
+const config = {
+  space: 'c8ty1si46b2l',
+  accessToken:
+    '5c21855ee21609b046041129a6eda8d847b2aef3cfae0249dfa258e8d72365d5'
+}
+
+module.exports = {
+  createClient() {
+    return contentful.createClient(config)
+  }
+}
