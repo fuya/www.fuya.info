b9716f3ad5ab5b097602f2ebbc6caac5d7af9381
Fuya <dev@fuya.info>
2019-01-27 15:01:28 +0900

    create MD component

---
+++ components/Markdown.vue
@@ -0,0 +1,25 @@
+<script>
+/* eslint-disable vue/no-v-html */
+import remark from 'remark'
+import html from 'remark-html'
+
+export default {
+  props: {
+    markdown: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    markdownHTML() {
+      return remark()
+        .use(html)
+        .processSync(this.markdown).contents
+    }
+  }
+}
+</script>
+
+<template>
+  <div v-html="markdownHTML" />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,21 @@
 <template>
   <section class="container">
-    {{ entry.fields.title }}
-    ---
-    {{ /* eslint-disable-next-line vue/no-v-html */}}
-    <div v-html="bodyHTML" />
+    <Markdown :markdown="entry.fields.bodyMd" />
   </section>
 </template>
 
 <script>
 /* eslint-disable no-console */
 import { createClient } from '~/plugins/contentful.js'
-import remark from 'remark'
-import html from 'remark-html'
+import Markdown from '~/components/Markdown'
 
 const client = createClient()
 
 export default {
-  components: {},
-  computed: {
-    bodyHTML() {
-      return remark()
-        .use(html)
-        .processSync(this.entry.fields.bodyMd).contents
-    }
-  },
+  components: { Markdown },
   head() {
     return {
-      title: this.entry,
-      meta: [
-        { hid: 'description', name: 'description', content: this.entry },
-        { hid: 'og:image', name: 'og:image', content: this.entry }
-      ]
+      title: this.entry.fields.title
     }
   },
   asyncData() {
@@ -40,6 +25,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
