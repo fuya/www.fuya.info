f7d05a68c2056b61a8af810df1f0493400dff5ec
Fuya <dev@fuya.info>
2019-01-27 15:01:43 +0900

    add Header/Footer components

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer :class="$style.footer">
+    <small :class="$style.copyRight">
+      Copyright &copy; 2019 Fuya.
+    </small>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" module>
+.footer {
+  margin: 2rem auto;
+  text-align: center;
+}
+.copyRight {
+  font-size: $tiny-font-size;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <header :class="$style.header">
+    <a href="/">
+      <h1 :class="$style.siteName">
+        Fuya.info
+      </h1>
+    </a>
+    <nav :class="$style.nav">
+      <a href="">
+        About
+      </a>
+      <a href="">
+        日記
+      </a>
+      <a href="">
+        snippets
+      </a>
+      <a href="">
+        リンク
+      </a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" module>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid $LIGHT_GRAY;
+  @include min-screen($WIDTH_S) {
+    padding: 0 1rem;
+  }
+  @include min-screen($WIDTH_M) {
+    padding: 0 2rem;
+  }
+}
+
+.siteName {
+  flex-shrink: 0;
+  font-size: $x-large-font-size;
+}
+
+.nav {
+  display: flex;
+  flex-shrink: 1;
+  > a {
+    margin: 0 0.5rem;
+  }
+}
+</style>
