0e5bc6a0867970d147b21b9e5a42329f7626fa95
Fuya <dev@fuya.info>
2019-01-27 23:43:20 +0900

    about me

---
+++ pages/about-me.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section>
+      <Markdown :markdown="entry.fields.bodyMd" />
+    </section>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+import { createClient } from '~/plugins/contentful.js'
+import Markdown from '~/components/Markdown'
+
+const client = createClient()
+
+export default {
+  components: { Markdown },
+  head() {
+    return {
+      title: this.entry.fields.title + '| Fuya.info'
+    }
+  },
+  asyncData() {
+    return client.getEntry('1ZQ894eJ8V0QsNHivH4STq').then(entry => ({
+      entry: entry
+    }))
+  }
+}
+</script>
