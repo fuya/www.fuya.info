2c20f5d74161c7f88853ba47119dd5fec3615085
Fuya <dev@fuya.info>
2019-01-29 22:45:50 +0900

    footnote

--- components/Markdown.vue
+++ components/Markdown.vue
@@ -22,5 +22,33 @@ export default {
 </script>
 
 <template>
-  <div v-html="markdownHTML" />
+  <div class="md" v-html="markdownHTML" />
 </template>
+
+<style lang="scss">
+.md {
+  counter-reset: number;
+
+  .footnote-ref {
+    position: relative;
+    top: -200px;
+    left: -1000px;
+    display: inline-block;
+    width: 1.8rem;
+    color: transparent;
+    counter-increment: number;
+    opacity: 1;
+
+    &:before {
+      position: relative;
+      top: 200px;
+      left: 1000px;
+      margin: 0 0.2rem;
+      font-size: $large-font-size;
+      font-weight: bold;
+      color: $DARK_SKY;
+      content: '[' counter(number) ']';
+    }
+  }
+}
+</style>
