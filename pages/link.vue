3cfa0e8774fff726a6679c07ed37ded23cd55697
Fuya <dev@fuya.info>
2019-02-03 11:50:44 +0900

    add link page

---
+++ pages/link.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section>
+      <Markdown :markdown="entry.fields.bodyMd" />
+    </section>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+import { createClient } from '~/plugins/contentful.js'
+import Markdown from '~/components/Markdown'
+
+const client = createClient()
+
+export default {
+  components: { Markdown },
+  head() {
+    return {
+      title: this.entry.fields.title + ' | Fuya.info'
+    }
+  },
+  asyncData() {
+    return client.getEntry('VyjGFIBVmXqGUPI5mjvjR').then(entry => ({
+      entry: entry
+    }))
+  }
+}
+</script>
