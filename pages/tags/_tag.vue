6d0198ae283ced959351406a9404118a76a3602a
Fuya <dev@fuya.info>
2019-02-16 15:18:39 +0900

    add Tag index page

---
+++ pages/tags/_tag.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>タグ 「{{ $route.params.tag }}」 を含む記事</h1>
+    <PostCard v-for="post in posts.items" :key="post.id" :post="post.fields" with-category />
+  </div>
+</template>
+
+<script>
+import PostCard from '~/components/PostCard'
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
+
+export default {
+  head() {
+    return {
+      title: `${this.pageTitle} | Fuya.info`,
+      link: [
+        {
+          hid: 'cannonical',
+          href: `https://fuya.info/tags/${this.$route.params.tag}`
+        }
+      ]
+    }
+  },
+  components: { PostCard },
+  async asyncData({ params, error, payload }) {
+    if (payload) {
+      return {
+        post: payload
+      }
+    }
+    const posts = await client.getEntries({
+      content_type: 'post',
+      'fields.tag': params.tag,
+      order: '-fields.publishAt'
+    })
+
+    return {
+      posts
+    }
+  },
+  computed: {
+    pageTitle: function() {
+      return `タグ 「${this.$route.params.tag}」を含む記事`
+    }
+  }
+}
+</script>
