76ba7cbd716d024625a31795fc301193b3204edf
Fuya <dev@fuya.info>
2019-04-13 11:28:30 +0900

    add works

---
+++ pages/old-valley.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section>
+      <Markdown :markdown="entry.fields.bodyMd" />
+    </section>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+import { createClient } from '~/plugins/contentful.js'
+import Markdown from '~/components/Markdown'
+
+const client = createClient()
+
+export default {
+  components: { Markdown },
+  head() {
+    return {
+      title: this.entry.fields.title + ' | Fuya.info'
+    }
+  },
+  asyncData() {
+    return client.getEntry('6R2ONst8fLaqdjlNpxi2Fj').then(entry => ({
+      entry: entry
+    }))
+  }
+}
+</script>
